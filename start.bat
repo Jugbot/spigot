db35278798bdfff37b64b85aedb9d44b3ee912d8
Lucas Pollice <lucasp465@gmail.com>
2021-01-02 18:53:24 -0500

    test

--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-java -Xms1G -Xmx3G -XX:+UseConcMarkSweepGC ^
--Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 ^
--jar spigot.jar nogui
-pause
--- start.sh
+++
@@ -1,3 +0,0 @@
-java -Xms1G -Xmx3G -XX:+UseConcMarkSweepGC \
--Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 \
--jar spigot.jar nogui
