3f83145d55bebe769921745cc78c2c8c91770754
Lucas Pollice <lp1935@nyu.edu>
2021-01-04 21:02:21 -0500

    qs

--- build.bat
+++ build.bat
@@ -1,7 +1,4 @@
 @echo off
-IF NOT EXIST BuildTools (
-    mkdir BuildTools
-)
 cd BuildTools
 curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 set /p Input=Enter the version: || set Input=latest
--- start.bat
+++ start.bat
@@ -2,4 +2,3 @@
 java -Xms1G -Xmx3G -XX:+UseConcMarkSweepGC ^
 -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 ^
 -jar spigot.jar nogui
-pause
