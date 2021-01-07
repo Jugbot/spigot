29719962dd523acbb1215d2162ddf67834880d5d
Lucas Pollice <lp1935@nyu.edu>
2021-01-06 20:26:22 -0500

    generate plugin sources

--- build.bat
+++ build.bat
@@ -1,24 +1,31 @@
 @echo off
 cd BuildTools
 curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-set /p Input=Enter the version: || echo Nothing entered, abort & exit 1
+set /p Input=Enter the version: || (echo Nothing entered, abort & exit 1)
 del spigot*.jar
 del craftbukkit*.jar
 java -jar BuildTools.jar --rev %Input%
 copy /b .\spigot*.jar ..\spigot.jar
 
 cd Bukkit
-mvn install
+call mvn install
 cd ..
 cd CraftBukkit
-mvn install
+call mvn install
 cd ..
 cd Spigot/Spigot-Api
-mvn install
+call mvn install
 cd ../..
 
 cd ../../plugin
-for /f "tokens=1,2 delims=." %%a in (%Input%) do (
+
+if not exist pom.xml (
+  cd archetype
+  call generate.bat
+  cd ..
+)
+
+for /f "tokens=1,2 delims=." %%a in ("%Input%") do (
   echo %%a%%b
-  mvn versions:set-property -Dproperty=spigot.api -DnewVersion=%%a%%b
+  mvn versions:set-property -Dproperty=spigot.api -DnewVersion=%%a.%%b
 )
